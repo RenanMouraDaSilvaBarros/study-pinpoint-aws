a0442c27244a8807cd4b3a6d3da47d914c2133cb
renan <naneraruom@gmail.com>
2022-06-19 20:48:52 -0300

    refactor

--- services/aws-service.js
+++ services/aws-service.js
@@ -1,26 +1,24 @@
-const AWS = require('aws-sdk')
+const AWS = require("aws-sdk");
 
 class AwsService {
+  static config() {
+    const myConfig = new AWS.Config();
+    myConfig.update({ region: "us-east-1" });
 
-    static config() {
-        const myConfig = new AWS.Config();
-        myConfig.update({ region: 'us-east-1' });
+    AWS.config.getCredentials(function (err) {
+      if (err) console.log(err.stack);
+      else {
+        console.log("succsess");
+      }
+    });
+  }
 
-        AWS.config.getCredentials(function (err) {
-            if (err) console.log(err.stack);
-            else {
-                console.log("succsess");
-            }
-        });
+  static profile(profile) {
+    const credentials = new AWS.SharedIniFileCredentials({profile: profile});
+    AWS.config.credentials = credentials
+    AWS.config.update({region:'us-east-1'});
 
-        
-    }
-
-    static profile(profile){
-        var credentials = new AWS.SharedIniFileCredentials({profile: profile});
-        console.log(credentials)
-    }
-
+  }
 }
 
 module.exports = AwsService;
